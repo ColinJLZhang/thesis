d9a19d5f5c95dc89cfe0768fee1ca5aa5a62de48
mrobotor <mrobotor@gmail.com>
2020-02-05 16:48:49 +0800

    ph3

--- code/randomimg.py
+++ code/randomimg.py
@@ -9,8 +9,27 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-img = np.random.randint(255, size=(512,521,3))
-# cv2.imshow("img",img)
-plt.imshow(img)
-print(img)
-plt.show()
+def randimg():
+	img = np.random.randint(255, size=(512,521,3))
+	# cv2.imshow("img",img)
+	plt.imshow(img)
+	print(img)
+	plt.show()
+
+def shufflepixel():
+	img = cv2.imread(r"../img/2_2_RAS.jpg")
+	# plt.figure()
+	# plt.imshow(img)
+	cv2.imshow("1",img)
+	size = img.shape
+	# img.flatten()
+	np.random.shuffle(img)
+	# new_img = np.resize(img, size)
+	# plt.figure()
+	cv2.imshow("2", img)
+	cv2.waitKey(0)
+	# plt.imshow(new_img)
+	# plt.show()
+
+if __name__ == '__main__':
+	shufflepixel()
